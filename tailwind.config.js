c52c6d86ef0758d7cb9bd85d57c2542f66175c81
jaglux12 <gonzalopez239@gmail.com>
2023-06-08 22:22:07 -0300

    modify buttons card proyect

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,12 @@ module.exports = {
                 'proyecto-4':'url(/assets/img/proyecto-4.png)',
                 'proyecto-5':'url(/assets/img/proyecto-5.png)' */
 
-                'profile':'url(https://i.postimg.cc/sg1XqV7q/perfil.png)',
+                /* 'profile':'url(https://i.postimg.cc/sg1XqV7q/perfil.png)',
                 'proyecto-1':'url(https://i.postimg.cc/MGCv1hc4/proyecto-1.png)',
                 'proyecto-2':'url(https://i.postimg.cc/MHxHFVmm/proyecto-2.png)',
                 'proyecto-3':'url(https://i.postimg.cc/J7cGBYR6/proyecto-3.png)',
                 'proyecto-4':'url(https://i.postimg.cc/7Zc6ypN6/proyecto-4.png)',
-                'proyecto-5':'url(https://i.postimg.cc/J0q0V0TV/proyecto-5.png)'
+                'proyecto-5':'url(https://i.postimg.cc/J0q0V0TV/proyecto-5.png)'*/
             },
             colors:{
                 'gray-oscuro':'#12131E',
